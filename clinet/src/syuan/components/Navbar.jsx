963b99b18d2ba058998693ddb19822c0f2d2d50b
Syuan <syuan1005>
2021-11-05 15:44:17 +0800

    修改footer位置+navbar下拉(還沒改完)

--- clinet/src/syuan/components/Navbar.jsx
+++ clinet/src/syuan/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavDropdown, Nav } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../components/css/Navbarcss.css";
+import "../components/css/dropdown.css";
 import Footer from "../components/Footer";
 
 function Navbar() {
@@ -37,6 +38,40 @@ function Navbar() {
             <Link to="/page1" className="navbar-brand">
               會員登入/註冊
             </Link>
+
+
+            {/* <nav class="nav_bar">
+              <ul class="nav_middle">
+
+                <li>
+                  <a href="#food_type" class="nav_menu">美食類型</a>
+                  <ul class="nav_menu_items">
+                    <li><a href="https://www.instagram.com/syuaneat/?hl=zh-tw" target="_blank">台式美食</a></li>
+                    <li><a href="https://www.instagram.com/syuaneat/?hl=zh-tw" target="_blank">日式美食</a></li>
+                    <li><a href="https://www.instagram.com/syuaneat/?hl=zh-tw" target="_blank">義式美食</a></li>
+                    <li><a href="https://www.instagram.com/syuaneat/?hl=zh-tw" target="_blank">咖啡廳</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#food_section" class="nav_menu">地區美食</a>
+                  <ul class="nav_menu_items">
+                    <li class="nav_menu2">
+                      <a href="https://www.instagram.com" target="_blank">北部美食</a>
+                      <ul class="nav_menu3">
+                        <li><a href="">基隆</a></li>
+                        <li><a href="">台北</a></li>
+                        <li><a href="">新北</a></li>
+                        <li><a href="">桃園</a></li>
+                        <li><a href="">新竹</a></li>
+                        <li><a href="">宜蘭</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav> */}
+
+
           </div>
         </div>
       </nav>
--- clinet/src/syuan/components/Navbarr.js
+++ clinet/src/syuan/components/Navbarr.js
@@ -7,6 +7,7 @@
 import '../components/css/Navcss.css';
 import { Container, Nav, Button, NavDropdown, Navbar, Form, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../components/css/dropdown.css";
 import {
     BrowserRouter as Router,
     Switch,
@@ -15,6 +16,14 @@ import {
 } from "react-router-dom";
 
 function Navbarr() {
+    const a = () => {
+        alert("ok");
+    }
+
+    const b = () => {
+        alert("ok1");
+    }
+
     return (
         <div>
             {/* <Router> */}
@@ -34,17 +43,102 @@ function Navbarr() {
                                     類型篩選
                                 </Link>
                                 <Nav.Link as={Link} to={"/page2"} className="navlink">縣市篩選</Nav.Link>
-                                {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action5">
-                                            Something else here
-                                        </NavDropdown.Item>
-                                    </NavDropdown> */}
-                                <Nav.Link as={Link} to={"/page1"} className="navlink">會員登入
+
+
+                                <NavDropdown title="類型篩選" id="navbarScrollingDropdown">
+
+                                    <NavDropdown.Item as={Link} to={"/page2"} className="dropitems">
+                                        <div>料理類型</div>
+                                        <button onClick={a}>小吃</button>
+                                        <button onClick={b}>燒烤</button>
+                                        <button>炸物</button>
+                                        <button>飲料</button>
+                                        <button>甜點</button>
+                                        <button>麵包</button><br />
+                                        <button>熱炒</button>
+                                        <button>禮盒</button>
+                                        <button>素食</button>
+                                        <button>壽司</button>
+                                        <button>丼飯</button>
+                                        <button>buffet</button><br />
+                                        <div>餐廳類型</div>
+                                        <button>中式</button>
+                                        <button>台式</button>
+                                        <button>韓式</button>
+                                        <button>日式</button>
+                                        <button>泰式</button>
+                                        <button>印尼</button><br />
+                                        <button>美式</button>
+                                        <button>法式</button>
+                                        <button>義式</button>
+                                        <button>印度</button>
+                                        <button>墨西哥</button>
+                                        <button>越南</button>
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+
+
+                                <NavDropdown title="縣市篩選" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item as={Link} to={"/page2"} className="dropitems">
+                                        <div>北部</div>
+                                        <button>臺北</button>
+                                        <button>新北</button>
+                                        <button>基隆</button>
+                                        <button>桃園</button>
+                                        <button>新竹</button>
+                                        <button>宜蘭</button>
+                                        <div>中部</div>
+                                        <button>苗栗</button>
+                                        <button>臺中</button>
+                                        <button>彰化</button>
+                                        <button>南投</button>
+                                        <button>雲林</button>
+                                        <div>南部</div>
+                                        <button>嘉義</button>
+                                        <button>臺南</button>
+                                        <button>高雄</button>
+                                        <button>屏東</button>
+                                        <div>東部</div>
+                                        <button>花蓮</button>
+                                        <button>臺東</button>
+                                        <div>離島</div>
+                                        <button>澎湖</button>
+                                        <button>金門</button>
+                                        <button>連江</button>
+                                        <button>小琉球</button>
+                                    </NavDropdown.Item>
+
+                                    {/* 範本：易軒用這個邏輯去寫，我改樣式 */}
+                                    {/* <NavDropdown.Item as={Link} to={"/page2"}>
+                                        <button onClick={a} variant="outline-warning">小吃</button>
+                                    </NavDropdown.Item> */}
+
+                                    {/* dropdown原檔備用 */}
+                                    {/* <NavDropdown.Item as={Link} to={"/page2"}>
+                                        Another action
+                                    </NavDropdown.Item> */}
+                                    {/* <NavDropdown.Divider /> */}
+                                    {/* <NavDropdown.Item href="#action5">
+                                        Something else here
+                                    </NavDropdown.Item> */}
+                                </NavDropdown>
+
+
+                                <NavDropdown title="會員" as={Link} to={"/page2"} id="navbarScrollingDropdown">
+                                    <NavDropdown.Item as={Link} to={"/page3"} className="dropitems">
+                                        <button>臺北</button>
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+
+
+
+                                <Nav.Link as={Link} to={"/page1"} className="navlink">會員功能
                                 </Nav.Link>
+
+
                             </Nav>
+
+                            {/* 搜尋按鈕 */}
                             <Form className="d-flex navdropDown">
                                 <FormControl
                                     type="search"
