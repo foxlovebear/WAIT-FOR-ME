f89ad51f2a928800a5ba3d955eaffab7b9b58ce2
dejavu92427 <dejavu92427@gmail.com>
2021-11-09 22:18:23 +0800

    評論>30顯示...

--- clinet/src/Pages/Page2/components/List.jsx
+++ clinet/src/Pages/Page2/components/List.jsx
@@ -9,7 +9,6 @@ function List() {
 
   return (
     <div id="restaurantRight">
-
       {data.map((v, i) => {
         return (
           <Container>
@@ -22,25 +21,61 @@ function List() {
                   <br />
                   <div>店名：{v.name}</div>
                   <div>電話：{v.phone}</div>
-                  <div>地址：{v.address}</div><br />
-                  <div>標籤：
-                    {v.city}&nbsp;{v.foodtype}&nbsp;{v.tag1}&nbsp;{v.tag2}&nbsp;{v.tag3}
+                  <div>地址：{v.address}</div>
+                  <br />
+                  <div>
+                    標籤：
+                    {v.city}&nbsp;{v.foodtype}&nbsp;{v.tag1}&nbsp;{v.tag2}&nbsp;
+                    {v.tag3}
                   </div>
                   {/* <p>{v.city}</p>
                     <p>{v.tag1}</p>
                     <p>{v.tag2}</p>
                     <p>{v.tag3}</p>
-
+                    <p>{v.foodtype}</p> */}
+                  {/* <p>{v.comment}</p> */}
+                </Col>
+                <Col className="col-2 page2comment">
+                  食客評論1：
+                  <br />
+                  <br />
+                  <p>{v.comment}</p>
+                </Col>
+                <Col className="col-2 page2comment">
+                  食客評論2：
+                  <br />
+                  <br />
+                  <p>{v.comment}</p>
+                </Col>
+                <Col className="col-2 page2comment">
+                  食客評論3：
+                  <br />
+                  <br />
+                  <p>{v.comment}</p>
+                </Col>
+              </Row>
+              <hr className="hrstyle" />
+              <p>{v.phone}</p>
+              <p>{v.address}</p>
+              <p>
+                {v.comment && v.comment.length > 30
+                  ? v.comment.substr(0, 30) + "..."
+                  : v.comment}
+              </p>
             </Link>
           </Container>
         );
       })}
 
-
       {/* 返回最上層按鈕BACKTOP */}
       <section className="backtop">
-        <a href="#" className="backtopa">TOP
-          <img src="/images/syuan/upbacktop.png" className="backtopimg" alt="" />
+        <a href="#" className="backtopa">
+          TOP
+          <img
+            src="/images/syuan/upbacktop.png"
+            className="backtopimg"
+            alt=""
+          />
         </a>
       </section>
     </div>
