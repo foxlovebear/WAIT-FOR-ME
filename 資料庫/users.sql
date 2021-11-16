74abd3ed45b6c692e486f7199ac53d3a97b0a58f
dejavu92427 <dejavu92427@gmail.com>
2021-11-16 10:20:05 +0800

    user資料表

--- 資料庫/users.sql
+++ 資料庫/users.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2021 年 11 月 11 日 11:29
--- 伺服器版本： 10.4.21-MariaDB
--- PHP 版本： 8.0.10
+-- 主機： localhost:8889
+-- 產生時間： 2021 年 11 月 16 日 02:18
+-- 伺服器版本： 5.7.34
+-- PHP 版本： 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,22 +37,22 @@ CREATE TABLE `users` (
   `phone` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `photo` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `role` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 傾印資料表的資料 `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `email`, `email_verified_at`, `password`, `address`, `phone`, `photo`, `created_at`, `updated_at`) VALUES
-(1, '狐狸1FOX1', 'fox1@gmail.com', '2021-10-31 16:00:00', '123', '狐狸星一號', '0999888777', '/userimg/1.jpg', '2021-10-29 16:00:00', '2021-10-30 16:00:00'),
-(2, '狐狸2FOX2', 'fox2@gmail.com', '2021-10-09 16:00:00', '123', '狐狸星二號', '0999777888', '/userimg/1.jpg', '2021-10-07 16:00:00', '2021-10-08 16:00:00'),
-(3, '狐狸3FOX3', 'fox3@gmail.com', '0000-00-00 00:00:00', '123', '狐狸星3號', '0999878787', '/userimg/3.jpg', '2021-10-05 16:00:00', '2021-10-06 16:00:00'),
-(4, '大狐狸', 'Bigfox@gmail.com', NULL, '1111', NULL, NULL, NULL, NULL, NULL),
-(5, '123', '123@gmail.com', NULL, '123', NULL, NULL, NULL, NULL, NULL),
-(6, 'syuan', 'syuan@gmail.com', NULL, '1111', NULL, NULL, NULL, NULL, NULL),
-(7, 'abc', 'abc@gmail.com', NULL, '1111', NULL, NULL, NULL, NULL, NULL);
-(8, 'admin', 'tsaijason@icloud.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`user_id`, `name`, `email`, `email_verified_at`, `password`, `address`, `phone`, `photo`, `created_at`, `updated_at`, `role`) VALUES
+(1, '狐狸1FOX1', 'fox1@gmail.com', '2021-10-31 16:00:00', '123', '狐狸星一號', '0999888777', '/userimg/1.jpg', '2021-10-29 16:00:00', '2021-10-30 16:00:00', 'user'),
+(2, '狐狸2FOX2', 'fox2@gmail.com', '2021-10-09 16:00:00', '123', '狐狸星二號', '0999777888', '/userimg/1.jpg', '2021-10-07 16:00:00', '2021-10-08 16:00:00', 'user'),
+(3, '狐狸3FOX3', 'fox3@gmail.com', '0000-00-00 00:00:00', '123', '狐狸星3號', '0999878787', '/userimg/3.jpg', '2021-10-05 16:00:00', '2021-10-06 16:00:00', 'user'),
+(4, '大狐狸', 'Bigfox@gmail.com', NULL, '1111', NULL, NULL, NULL, NULL, NULL, 'user'),
+(5, '123', '123@gmail.com', NULL, '123', NULL, NULL, NULL, NULL, NULL, 'user'),
+(6, 'syuan', 'syuan@gmail.com', NULL, '1111', NULL, NULL, NULL, NULL, NULL, 'user'),
+(13, 'admin', 'tsaijason@icloud.com', NULL, 'admin', NULL, NULL, NULL, NULL, NULL, 'admin');
 
 --
 -- 已傾印資料表的索引
@@ -72,7 +72,7 @@ ALTER TABLE `users`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
