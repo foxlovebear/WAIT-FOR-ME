987f3247ca03baf0cc2a035185f4310bc82d6d0b
Alicia Hsiang <31757903+foxlovebear@users.noreply.github.com>
2021-11-16 12:52:37 +0800

    Revert "會員頁可查閱過往評論、更換頭像"

--- clinet/src/Chi/pages/MemberPage/conponents/CommemtList.js
+++ clinet/src/Chi/pages/MemberPage/conponents/CommemtList.js
@@ -9,29 +9,32 @@ import {Container,Row,Col,Stack,
  Button } from 'react-bootstrap';
 //使用圖片
 import FMemberIcon from './MemberIcon.js';
-import FUsersStoreComment from './UsersStoreComment';
+
 //
 function FCommemtList(){
-    // const id = localStorage.getItem("id");
-    // const authShowComment=()=>{  
-    //     console.log(id);
-    //     Axios.post('http://localhost:3001/showUserComment',{
-    //       headers:{"x-access-token":localStorage.getItem("token"),},
-    //       id:id,
-    //     }).then((response)=>{
-    //       if(response.data.auth == false){
-    //         document.location.href="/login";
-    //       }
-    //       console.log(response);
-    //       if(response.data.length>0){
-    //       localStorage.setItem("comment1",response.data[0].comment);
-    //       }
-    //     })
-    // }
-
+    const id = localStorage.getItem("id");
+    const authShowComment=()=>{  
+        console.log(id);
+        Axios.post('http://localhost:3001/showUserComment',{
+          headers:{"x-access-token":localStorage.getItem("token"),},
+          id:id,
+        }).then((response)=>{
+          if(response.data.auth == false){
+            document.location.href="/login";
+          }
+          console.log(response);
+          if(response.data.length>0){
+          localStorage.setItem("comment1",response.data[0].comment);
+          }
+        })
+    }
+  var a = Promise.resolve("okla");
+  a.then(res=>console.log(res));
 
   return( 
             <Container className="userCommentContainer mb-5 ">
+                          {authShowComment()}
+
             {/* 個人資訊+店家 */}
             <Row id="commentUserInfo" >
               {/* 用戶小標+用戶名 */}
@@ -39,7 +42,6 @@ function FCommemtList(){
                 <Col lg={5} className="userIcon w-100"><FMemberIcon/></Col>
                 <Col className="commenterName"><h6>{localStorage.getItem('name')}</h6></Col>
               </Col>  
-
               <Col lg={8} className="CommentStoreInfo">
                   <Stack>
                     <Col><h6>2021/10/28 </h6></Col>
@@ -50,7 +52,15 @@ function FCommemtList(){
               </Col>
             </Row>
             {/* 評論內容 */}
-            <FUsersStoreComment/>
+            <Row>
+              <Col lg={4}></Col>
+              <Col className="commentText">(評論內容:){localStorage.getItem('comment1')}</Col>
+            </Row>
+            {/* 圖片 */}
+            <Row>
+              <Col lg={4}></Col>
+              <Col lg={8}>圖片<img src="https://picsum.photos/id/102/300/250"/></Col>
+            </Row>
         </Container>
 
   )
--- clinet/src/Chi/pages/MemberPage/conponents/MemberIcon.js
+++ clinet/src/Chi/pages/MemberPage/conponents/MemberIcon.js
@@ -7,12 +7,12 @@ import {Container,Row,Col,Stack,
  FormControl,
  Button } from 'react-bootstrap';
 //使用圖片
-import female from '../img/female.png';
+import Me from '../img/Me.jpg';
 import EmptyHead from '../img/EmptyHead.jpg';
 
 function FMemberIcon(){
   return(
-    <img src={localStorage.getItem('name') == null?EmptyHead:female} alt="Me"className="rounded-circle w-50" />
+    <img src={localStorage.getItem('name') == null?EmptyHead:Me} alt="Me"className="rounded-circle w-50" />
   )
 }
 
