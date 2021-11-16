1ec042f4f39748d1499590d3c983b2a3bb07f2b0
Alicia_Hsiang <b01101049@gmail.com>
2021-11-16 18:07:21 +0800

    會員頁 能查看過往評論+更換女頭像圖

--- clinet/src/Chi/pages/MemberPage/conponents/MemberIcon.js
+++ clinet/src/Chi/pages/MemberPage/conponents/MemberIcon.js
@@ -7,12 +7,12 @@ import {Container,Row,Col,Stack,
  FormControl,
  Button } from 'react-bootstrap';
 //使用圖片
-import Me from '../img/Me.jpg';
+import female from '../img/female.png';
 import EmptyHead from '../img/EmptyHead.jpg';
 
 function FMemberIcon(){
   return(
-    <img src={localStorage.getItem('name') == null?EmptyHead:Me} alt="Me"className="rounded-circle w-50" />
+    <img src={localStorage.getItem('name') == null?EmptyHead:female} alt="Me"className="rounded-circle w-50" />
   )
 }
 
