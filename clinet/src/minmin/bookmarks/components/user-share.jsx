c83064f3cacb63630f0e8a5148505fe33ac38623
minxmin0710 <poing85467@gmail.com>
2021-11-19 18:29:01 +0800

    11/19敏敏星星顏色修正需跑指令喔!!

--- clinet/src/minmin/bookmarks/components/user-share.jsx
+++ clinet/src/minmin/bookmarks/components/user-share.jsx
@@ -1,6 +1,6 @@
-import React, { Component , useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table, Card, Row, Col, Container } from 'react-bootstrap';
+import {  Row, Col, Container } from 'react-bootstrap';
 
 // 引用用戶頭像照片
 import Woman from './icon/woman.png';
@@ -9,14 +9,14 @@ import Female from './icon/female.png';
 // 連接資料
 import Axios from 'axios';
 // 星星
-import { FaStar } from 'react-icons/fa';
+// import { FaStar } from 'react-icons/fa';
 
-// const bgStyle ={
-//     width:"90px",
-//     height:'90px'
+import { css, ThemeProvider } from "styled-components";
+import { base, Star } from "grommet-icons";
+import { deepMerge } from "grommet-icons/utils";
 
-// }
-// style={bgStyle} 
+
+
 
 const starDesign = {
     marign: "20px",
@@ -26,9 +26,20 @@ const starDesign = {
 function Share({ data }) {
 
     const [ReviewList, setList] = useState([]);
-    const stars = Array(4).fill();
 
 
+
+    const customColorTheme = deepMerge(base, {
+        icon: {
+            extend: css`
+        ${(props) =>
+                    props.color === "golden" &&
+                    `
+        fill: #FFDC35;
+      `}
+      `
+        }
+    });
 
 
 
@@ -57,9 +68,11 @@ function Share({ data }) {
                             超級好吃~下次要帶家人來吃超級好吃~
                             <br />
                             <br />
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
+                            <ThemeProvider theme={customColorTheme}>
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                            </ThemeProvider>
                         </div>
                     </Col>
 
@@ -76,10 +89,12 @@ function Share({ data }) {
                         <div className="text-dark text-align"   >
                             還不錯的咖啡廳，推薦黑糖卡布奇諾，早午餐份量十足，味道也很好，千層馬鈴薯值得一試，店裡貌似還可以看電影，如果是時間有餘裕，可以再來，因為出餐時間略久，著急的人不適合。
                             <br />
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
+                            <ThemeProvider theme={customColorTheme}>
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                            </ThemeProvider>
                         </div>
                     </Col>
 
@@ -96,9 +111,14 @@ function Share({ data }) {
                             甜點可選自製可麗露、美翻天菊花豆腐等，可喫茶、食材跟甜點都很有質感的台北餐廳推薦
                             <br />
                             <br />
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
-                            <FaStar style={starDesign}size={25}/>
+                            <ThemeProvider theme={customColorTheme}>
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                            </ThemeProvider>
+
+
                         </div>
                     </Col>
 
@@ -116,16 +136,18 @@ function Share({ data }) {
                             非常驚艷，餐點特別，裝潢時尚，特別推薦海鮮跟甜點鍋餅，紅酒🍷櫻桃鴨非常特別，滿滿的酒香
                             <br />
                             <br />
-                            <FaStar style={starDesign} size={25} />
-                            <FaStar style={starDesign} size={25} />
-                            <FaStar style={starDesign} size={25} />
+                            <ThemeProvider theme={customColorTheme}>
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                                <Star color="golden" size="medium" />
+                            </ThemeProvider>
                         </div>
                     </Col>
 
                 </Row>
                 {/* 其他會員的假資料，用寫死的畫面呈現 */}
 
-                
+
                 <Row className="border border-warning p-2 " >
                     <Col className="border border-warning p-1 bg-warning  " xs={3} xl={1}  >
 
@@ -141,20 +163,14 @@ function Share({ data }) {
 
 
                         <br />
-                        {stars.map((_, index) => {
-                            return (
-                                <FaStar
-                                    style={starDesign}
-                                    color={"#FFDC35"}
-                                    size={30}
-                                    key={index}
-                                />
-                            )
-                        }
+                        <ThemeProvider theme={customColorTheme}>
+                            <Star color="golden" size="medium" />
+                            <Star color="golden" size="medium" />
+                            <Star color="golden" size="medium" />
+                            <Star color="golden" size="medium" />
+                        </ThemeProvider>
 
-                        )}
 
-                     
 
 
 
